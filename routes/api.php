20c4a8a2dcc8f6e76d9c475174d928f7cd6e8aac
jmakinin <36607363+jmakinin@users.noreply.github.com>
2023-05-24 07:34:56 +0000

    route: /department create: post

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 use App\Http\Controllers\MembersController;
+use App\Http\Controllers\DepartmentsController;
 
 Route::get('/member', function (){
     return 'false';
 });
 Route::post('/members', [MembersController::class, 'create']);
+Route::post('/departments', [DepartmentsController::class, 'create']);
