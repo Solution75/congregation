01a153bcf01def1daf264c0f375f7b8edfcced6b
jmakinin <36607363+jmakinin@users.noreply.github.com>
2023-05-24 07:37:10 +0000

    feat: department migration

--- database/migrations/2023_05_22_125932_create_departments_table.php
+++ database/migrations/2023_05_22_125932_create_departments_table.php
@@ -12,7 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('departments', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
+            $table->string('name')->unique();
+            $table->string('savedBy');
+            $table->dateTimeTz('createdAt')->default(now());
+            $table->dateTimeTz('updatedAt')->nullable();
+
             $table->timestamps();
         });
     }
